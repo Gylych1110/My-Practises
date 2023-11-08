d2303ec6a854b394f84d270ac52433014750031d
Gylych1110 <149843995+Gylych1110@users.noreply.github.com>
2023-11-08 23:47:39 +0100

    Add files via upload

---
+++ G1.java
@@ -0,0 +1,6 @@
+public class G1 {
+    public static void main(String[] args) {
+        System.out.println("Gylych 1 printing");
+        System.out.println("Hello Java");
+    }
+}
---
+++ G2.java
@@ -0,0 +1,7 @@
+public class G2 {
+    public static void main(String[] args) {
+        System.out.println("2nd Folder Created");
+
+        System.out.println("Do what do you want");
+    }
+}
